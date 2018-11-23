6aac33ae1164ef5ec3f7b31d0d3fd6243aaa48a1
memoryempty <45251656+memoryempty@users.noreply.github.com>
2018-11-23 17:42:31 -0300

    Add files via upload

--- smb_logintest.py
+++ smb_logintest.py
@@ -3,7 +3,7 @@
 
 # Módulos
 import argparse
-import clr
+import clr.clr as clr
 import pysmb
 
 #======CLR IMPORT=====================
@@ -22,12 +22,13 @@ ast = cw+"["+tt+"*"+c+cw+"]"+cw
 
 parser = argparse.ArgumentParser(description='Test access to samba server')
 parser.add_argument('--port', type=int, default='445', help='Port to connect. Default is 445')
-parser.add_argument('-u', '--username', type=str, default='Guest', help='Username. Default is Guest')
-parser.add_argument('-p', '--password', type=str, default='passwd', help='Password. Default is "passwd"')
-parser.add_argument('-i', '--ip', type=str, required=True, help='server ip. example: 192.168.0.1')
-parser.add_argument('-s', '--server', type=str, required=True, help='server name. example: 192.168.0.1//sharename')
-parser.add_argument('-f', '--sharename', type=str, required=True, help='Set the folders to test.')
+parser.add_argument('-u', '--Username', type=str, default='Guest', help='Username. Default is Guest')
+parser.add_argument('-p', '--Password', type=str, default='passwd', help='Password. Default is "passwd"')
+parser.add_argument('-i', '--Ip', type=str, required=True, help='server ip. example: 192.168.0.1')
+parser.add_argument('-n', '--Name', type=str, required=True, help='server name. example: 192.168.0.1//NAME')
+parser.add_argument('-f', '--Sharename', type=str, help='Set the folders to test.')
 args = parser.parse_args()
+smbsrv = "smb//"+Ip+"//"+Name
 # Clases
 # ---------------------------------------------------------------------
 
@@ -36,11 +37,34 @@ args = parser.parse_args()
 
 # Funciones
 # ---------------------------------------------------------------------
+def smb_folders():
+	if Sharename:
+		try:
+			print ok+" Connecting to "+smbsrv+"..."+c
+			f = open(filetmp, 'w')
+		#conectar al servidor
+		#Guardar en archivo	
+		#optener Folders
+			for line in f.readlines():
+				folder = line.strip(columna) #fixme
+		#guardar en lista 
+				Sharename.append(folder)
+			print ok+" List of Folders in "+cg+smbsrv+cw+" updated"+c
+			print ok= "Folders: "+cg+Sharename+c
+			f.close()
+		except Exception, e:
+			print err+" Error " +str(e)
+	else:
+		print ok+" Using only assigned Folders Names: "+Sharename+c
+	except Exception, e:
+		print err+" Folders Names invalid?"+c
+		
 def smb_server_test():
-	print ok+cw+"Running"+tt+" smb server test"+c
-	print (cw+ok+"connecting to "+cg+"{f}".format(f=args.sharename)+cw+" at server "+cg+"{s}".format(s=args.server)+cw+", port "+cg+"{p}".format(p=args.port)+c)
+	smb_folders()
+	print ok+" Running"+tt+" smb server test..."+c
+	print (ok+" Connecting to "+cg+"{f}".format(f=args.Sharename)+cw+" at server "+cg+"{s}".format(s=args.Name)+cw+", port "+cg+"{p}".format(p=args.Port)+"..."+c)
 	try:
-		conn = SMBConnection(username, password, 'client_machine_name', server_name, use_ntlm_v2 = True)
+		conn = SMBConnection(username, password, 'client_machine_name', server, use_ntlm_v2 = True)
 		assert conn.connect(ip, port)
 	except Exception, e:
 			print err+" Error " +str(e)
